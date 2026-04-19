c7c5b7823da4ee421b65b6a20d3155fe76af5d13
Entink <entink.mc@gmail.com>
2026-04-20 01:17:05 +0200

    .

--- Assets/Scripts/EndTrigger.cs
+++ Assets/Scripts/EndTrigger.cs
@@ -10,10 +10,11 @@ public class EndTrigger : MonoBehaviour
         if (endScreenUI != null)
         {
             endScreenUI.SetActive(false);
+            
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Player"))
